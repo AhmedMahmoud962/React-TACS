4ee7ddc776e2225e8cc7440a0b4a17c3c763a77c
AhmedMahmoud962 <am6944430@gmail.com>
2024-01-25 19:20:31 +0200

    Tacs-Project

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,33 @@
+const MenuItems = [
+  {
+    title: "Home",
+    url: "/",
+    ClName: "nav-links",
+    icon: "fa-solid fa-house-user",
+  },
+  {
+    title: "About",
+    url: "/about",
+    ClName: "nav-links",
+    icon: "fa-solid fa-circle-info",
+  },
+  {
+    title: "Service",
+    url: "/service",
+    ClName: "nav-links",
+    icon: "fa-solid fa-briefcase",
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+    ClName: "nav-links",
+    icon: "fa-solid fa-address-book",
+  },
+  {
+    title: "Sign up",
+    url: "/signup",
+    ClName: "nav-links-mobile",
+  },
+];
+
+export default MenuItems;
